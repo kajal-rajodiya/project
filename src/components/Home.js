48bfc1977a803e4b3823f2cadcfce36bda1b3fb4
Kajal-rajodiya <kajalrajodiya1@gmail.com>
2024-03-20 14:36:01 +0530

    commit changes

---
+++ src/components/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import {  Container,Button} from "reactstrap";
+const Home=()=>{
+    return (
+        <div>
+       <h1>Learn code with Durgesh</h1>
+       <p>
+        This is developer by learncode with Durgesh for learning purpose of backend is on
+         spring boot and frontend on react js
+       </p>
+        <Container>
+            <Button color="primary" outline>
+                Start Using
+            </Button>
+        </Container>
+        </div>
+    )
+};
+export default Home;
